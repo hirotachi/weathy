96be257ffef337a7aec099e72cd260dba7c4d8c0
hirotachi <milesfiles0@gmail.com>
2019-01-10 16:51:46 +0100

    fix path not imported on server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {ApolloServer} from "apollo-server-express";
+import path from "path";
 import cors from "cors";
 
 import typeDefs from "./schema";
 import resolvers from "./resolvers/resolvers";
 
-
+const publicPath = path.join(__dirname, "public");
 const server = new ApolloServer({
   typeDefs,
   resolvers
@@ -17,7 +18,7 @@ server.applyMiddleware({app});
 const port = process.env.PORT || 3000;
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"))
+  res.sendFile(path.join(publicPath, "index.html"))
 });
 app.listen(port, () => {
   console.log(`Server listening and graphql on ${server.graphqlPath}`);
